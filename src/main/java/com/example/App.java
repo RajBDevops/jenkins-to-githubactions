ea38ab214688c6a755306386df6ef85de84f984a
RajBDevops <144301897+RajBDevops@users.noreply.github.com>
2025-08-13 11:50:15 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class App {
+    public static void main(String[] args) {
+        String message = "Hello from Maven!";
+        System.out.println(StringUtils.upperCase(message));
+    }
+}
