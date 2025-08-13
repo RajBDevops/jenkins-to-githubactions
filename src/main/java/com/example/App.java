b1345623605867f17b3984179a1cb406af72abbf
RajBDevops <144301897+RajBDevops@users.noreply.github.com>
2025-08-13 12:10:39 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,7 @@
 package com.example;
 
-import org.apache.commons.lang3.StringUtils;
-
 public class App {
     public static void main(String[] args) {
-        String message = "Hello from Maven!";
-        System.out.println(StringUtils.upperCase(message));
+        System.out.println("Hello from GitHub Actions!");
     }
 }
